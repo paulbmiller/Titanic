bad599509ac98e1d3e57cb65adc93155a4420dc7
Paul Miller <miller_p@hotmail.fr>
2019-12-21 01:20:23 +0100

    Completed preprocessing

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import scipy.cluster.hierarchy as sch
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import StandardScaler
+import torch.nn as nn
+from torch.utils.data import DataLoader, TensorDataset
 
 training_set = pd.read_csv('train.csv')
 test_set = pd.read_csv('test.csv')
@@ -67,6 +70,13 @@ test_set = pd.get_dummies(test_set, columns=['Name'])
 training_set = training_set.drop('Name_Mr', axis=1)
 test_set = test_set.drop('Name_Mr', axis=1)
 
+# Encode Pclass into categorical features and avoid dummy variable trap
+training_set = pd.get_dummies(training_set, columns=['Pclass'])
+test_set = pd.get_dummies(test_set, columns=['Pclass'])
+training_set = training_set.drop('Pclass_3', axis=1)
+test_set = test_set.drop('Pclass_3', axis=1)
+
+
 # Get the number of cabins and suppose NaN values mean 1 cabin
 training_set.Cabin = training_set.Cabin.fillna(1)
 training_set.loc[training_set.Cabin != 1, 'Cabin'] = \
@@ -76,38 +86,28 @@ test_set.Cabin = test_set.Cabin.fillna(1)
 test_set.loc[test_set.Cabin != 1, 'Cabin'] = \
     test_set.loc[test_set.Cabin != 1, 'Cabin'].apply(lambda x:
                                                      len(x.split(' ')))
-
-# Drop the ticket column
+        
+# Drop the passenger id and ticket columns
+training_set = training_set.drop('PassengerId', axis=1)
 training_set = training_set.drop('Ticket', axis=1)
+test_set = test_set.drop('PassengerId', axis=1)
 test_set = test_set.drop('Ticket', axis=1)
 
-# Get the data used for clustering
-# Note: Maybe we should include Cabin position to include the probability
-# of the person being at that spot (but most of these are not specified)
-train_indices_clustering = np.array([2, 4, 5, 6, 7, 9, 10, 11, 12], dtype=int)
-train_clust = training_set.iloc[:, train_indices_clustering]
-test_clust = test_set.iloc[:, train_indices_clustering -
-                           np.ones(len(train_indices_clustering))]
-
-# =============================================================================
-# dendrogram = sch.dendrogram(sch.linkage(train_clust, method='ward'))
-# plt.title('Dendrogram')
-# plt.xlabel('People')
-# plt.ylabel('Euclidian distances')
-# plt.show()
-# =============================================================================
+# Use Random Forest Regression to predict the missing ages of passengers
+reg = RandomForestRegressor(n_estimators=1000)
+reg.fit(training_set[training_set.Age.notna()].loc[:, training_set.columns != 'Age'],
+        training_set.Age[training_set.Age.notna()])
+training_set.loc[training_set.Age.isna(), 'Age'] = \
+    reg.predict(training_set[training_set.Age.isna()].loc[:, training_set.columns != 'Age'])
+test_set.loc[test_set.Age.isna(), 'Age'] = \
+    reg.predict(test_set[test_set.Age.isna()].loc[:, test_set.columns != 'Age'])
 
 # Scale data
 sc = StandardScaler()
-training_set = sc.fit_transform(training_set)
-test_set = sc.transform(test_set)
+training_set.loc[:, ['Age', 'Fare']] = sc.fit_transform(training_set.loc[:, ['Age', 'Fare']])
+test_set.loc[:, ['Age', 'Fare']] = sc.transform(test_set.loc[:, ['Age', 'Fare']])
 
-# Use Random Forest Regression to replace NaN values for Age using indices
-# [2, 4, 6, 7, 9, 10, 11, 12]
-indices = np.array([2, 4, 6, 7, 9, 10, 11, 12], dtype=int)
-X_train = training_set.iloc[:, indices]
-y_train = training_set.iloc[:,]
-X_test = training_set.iloc[:, indices]
+
 
 
 
